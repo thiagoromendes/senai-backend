d77801681b57e13a92b87331bde3f6a0e8b89c68
Thiago Mendes <thiagoromendes@gmail.com>
2021-02-09 13:53:53 -0300

    feat: initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.get('/', (req,res) => {
+    res.send('Servidor rodando!!!')
+})
+
+app.listen(port,() => {console.log(`Servidor rodando na porta: ${port}`)})
